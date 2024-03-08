00b4f5c4422e5d9bd70f11bd9c44080f1a541475
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-03-08 14:05:16 +0100

    post

--- api/routes/productRoute.js
+++ api/routes/productRoute.js
@@ -22,7 +22,7 @@ route.get("/", productController.getAllProduct)
 route.post("/create", productController.createproduct)
 route.post("/show", productController.show)
 route.post("/delete", productController.deleteProduct)
-route.get("/search-product", productController.searchProduct)
+route.post("/search-product", productController.searchProduct)
 route.post("/vendor-product", productController.getProductByOwner)
 
 module.exports = route
